2390e015f9d8462cf276e2f5c7eb26a9d0a7cf57
Yago Muller <110571708+yagommuller@users.noreply.github.com>
2023-07-09 02:38:02 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const personagem = document.querySelectorAll('.personagem');
+
+personagem.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () =>{
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        
+        personagem.classList.add('selecionado');
+
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+    })
+})
